0f835ffce2bd767d4e6ea9061e6ceca66b3ee0ae
manaal <manaal.basabra@student.moringaschool.com>
2022-08-06 17:07:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+const number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+
+function countDown (number) {
+
+    let countDown = 10;
+    while (number < 10) {
+        console.log(number--);
+        debugger;
+
+    }
+    return number;
+}
+countDown(number);
+
